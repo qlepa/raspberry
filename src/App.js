084e188ab03536683be1a58dd0786e33c85c0171
Tomasz Klepacki <t.klepacki@gmail.com>
2018-12-13 23:29:58 +0100

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Main from './Components/Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello people! or not...</h1>
+      <div className="container">
+        <Main />
       </div>
     );
   }
---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+
+   render(){
+     return (
+        <div>
+            <h1>Rapsberry kingdom</h1>
+            <button>ENTER THE GATES</button>
+        </div>
+     )
+   }
+ }
+ export default Main
