e0b706a030f3bce7c9ed1524caaf653b7d506853
Tomasz Klepacki <t.klepacki@gmail.com>
2019-07-13 08:59:05 +0200

    Updates

---
+++ precache-manifest.99d2c3420c26a89066f3a68dd0899d89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "435895e6320297c18939",
+    "url": "/raspberry/static/css/main.08f3e25d.chunk.css"
+  },
+  {
+    "revision": "435895e6320297c18939",
+    "url": "/raspberry/static/js/main.435895e6.chunk.js"
+  },
+  {
+    "revision": "ce8860c512edbb0bfc6a",
+    "url": "/raspberry/static/js/1.ce8860c5.chunk.js"
+  },
+  {
+    "revision": "d0272cec7d841f062cd6",
+    "url": "/raspberry/static/js/runtime~main.d0272cec.js"
+  },
+  {
+    "revision": "b623d833694265eafaa17806c6b68ef0",
+    "url": "/raspberry/static/media/bakery-webfont.b623d833.woff2"
+  },
+  {
+    "revision": "44098550e9c1c1314659acdb71d084e7",
+    "url": "/raspberry/static/media/bakery-webfont.44098550.woff"
+  },
+  {
+    "revision": "d51e46594ef0177c6ba3b978b51090df",
+    "url": "/raspberry/static/media/raspberry-bckg.d51e4659.png"
+  },
+  {
+    "revision": "ae63172d7e0e99ef0e503d636e32aa7b",
+    "url": "/raspberry/index.html"
+  }
+];
